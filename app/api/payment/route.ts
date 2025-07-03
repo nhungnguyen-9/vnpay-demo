ac865a14db8b3748f0b0219e76006c81ed32fc32
nhungnguyen-9 <nguyennhung9846@gmail.com>
2025-07-03 19:40:41 +0700

    first commit

---
+++ app/api/payment/route.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from "next/server";
+import qs from 'qs'
+
+export async function POST(req: NextRequest) {
+    const body = await req.json()
+    const amount = body.amount
+
+    if (!amount || isNaN(amount)) {
+        return NextResponse.json({ error: 'Invalid amount' }, { status: 400 })
+    }
+
+    const vnp_TmnCode = process.env.VNP_TMN_CODE
+    const vnp_HashSecret = process.env.VNP_HASH_SECRET
+    const vnp_Url_Sandbox = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'
+    const vnp_ReturnUrl = 'http://localhost:3000/vnp/payment_return'
+
+    const date = new Date()
+    // const createDate = `${date.getFullYear()}${pad(date.getMonth() + 1)}${pad(date.getDate())}${pad(date.getHours())}${pad(date.getMinutes())}${pad(date.getSeconds())}`;
+    const orderId = Date.now().toString()
+
+    let locale = body.language
+    if (locale === null || locale === '') {
+        locale = 'vn'
+    }
+
+    let ipAddr = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+
+    let vnp_Params: Record<string, string> = {
+        vnp_Version: '1.0.0',
+        vnp_Command: 'pay',
+        vnp_TmnCode,
+        vnp_Locale: locale,
+        vnp_CurrCode: 'VND',
+        vnp_TxnRef: orderId,
+        vnp_OrderInfo: `Payment of order ${orderId}`,
+        vnp_OrderType: 'other',
+        vnp_Amount: (amount * 100).toString(),
+        vnp_ReturnUrl,
+        vnp_IpAddr: ipAddr,
+        vnp_CreateDate: createDate,
+    }
+
+    vnp_Params = sortObject(vnp_Params);
+
+    const signData = qs.stringify(vnp_Params, { encode: false })
+    const hmac = crypto.createHmac('sha512', vnp_HashSecret);
+    const secureHash = hmac.update(Buffer.from(signData, 'utf-8')).digest('hex');
+
+    vnp_Params['vnp_SecureHash'] = secureHash;
+
+    const paymentUrl = `${vnp_Url_Sandbox}?${qs.stringify(vnp_Params, { encode: true })}`;
+
+    return NextResponse.json({ url: paymentUrl });
+
+}
